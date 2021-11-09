ff0521c0a3b14033b212030cd3a3acf3f0e71e38
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-11-09 19:19:58 +0100

    documentation TCP/IP

--- server.cpp
+++ server.cpp
@@ -1,44 +1,64 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-void	exit_error2(std::string msg){
-	std::cout << "Error" << std::endl << msg << std::endl;
-	exit(1);
+void	exit_error(std::string error)
+{
+	std::cout << "Error: "<< error << std::endl;
+	exit(EXIT_FAILURE);
 }
+
+/*
+**	Server TCP / IP
+** Socket()
+** Bind()
+** Listen()
+** Accept()
+** Write() / Read()  [communication server - client]
+** Close()
+*/
 
 int	main(int ac, char **av)
 {
-	int	serv_fd, new_socket, rd;
-	//int	opt_val = 1;
-	int	addrlen;
-	char buffer[1024] = {0};
-	int port;
-	struct sockaddr_in	address;
+	int					server_socket = 0; 	// socket server 
+	int					port;				// server port
+	int					new_socket;			// socket client -> accept()
+	socklen_t			addrlen;			// sizeof la structure sockaddr_in
+	struct sockaddr_in	t_sockaddr_in;		// structure, données pour bind(), accept(), etc
+	char				buffer[1024];
 
 	if (ac != 2)
-		exit_error2("Usage: ./client port");
+		exit_error("./server [PORT]");
+	if(!(server_socket = socket(AF_INET, SOCK_STREAM, 0)))
+		exit_error("21: Socket Error");
+
 	port = atoi(av[1]);
-	serv_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (serv_fd == 0)
-		exit_error2("Socket creation failed");
-	// if (setsockopt(serv_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt_val, sizeof(opt_val)))
-	//	 exit_error2("Failed attaching socket");
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_family = AF_INET;
-	address.sin_port = htons(port);
-	if ((bind(serv_fd, (struct sockaddr *)&address, sizeof(address))) < 0)
-		exit_error2("Failed attaching socket to the port");
-	if (listen(serv_fd, 3) < 0)
-		exit_error2("Failed set soket to passive socket");
-	addrlen = sizeof(address);
-	while (1) {
-		new_socket = accept(serv_fd, (struct sockaddr *)&address, (socklen_t *)&(addrlen));
-		if (new_socket < 0)
-			exit_error2("Failed to add client");
-		rd = recv(new_socket, buffer, 1024, 0);
-		std::cout << "Message received by " << (address.sin_addr.s_addr) << ": " << buffer << std::endl;
-	}
+	addrlen = sizeof(t_sockaddr_in);
+
+	/* Remplire la structure pour donner les informations au bind */
+	t_sockaddr_in.sin_port				= port;			// port
+	t_sockaddr_in.sin_addr.s_addr		= INADDR_ANY;	// n'importe quelle address ?
+	t_sockaddr_in.sin_family			= AF_INET;		// famille ipv4
+
+	/* Cast de la grande structure sockaddr_in avec une plus petite structure sockaddr */
+	if (bind(server_socket, (struct sockaddr *)&t_sockaddr_in, addrlen) == -1)
+		exit_error("25: Bind Error");
+
+
+	// ==================================== micka et kevin part
+
+	// if (listen(server_socket, 3) < 0)
+	// 	exit_error("Failed set soket to passive socket");
+	// while (1) {
+	// 	new_socket = accept(server_socket, (struct sockaddr *)&t_sockaddr_in, &addrlen);
+	// 	if (new_socket < 0)
+	// 		exit_error("Accept client error");
+	// 	recv(new_socket, buffer, 1024, 0);
+	// 	std::cout << "Message received by " << (t_sockaddr_in.sin_addr.s_addr) << ": " << buffer << std::endl;
+	// }
+
 }
