ca94e433ad6a2b8b3e543f0d6e82b7ed8db31b62
Florent Gomez <fgomez@c2r4p1.42nice.fr>
2021-11-22 17:45:11 +0100

    CMD PASS done

--- srcs/commands/help.cpp
+++ srcs/commands/help.cpp
@@ -17,20 +17,3 @@ void	cmd_help(client cl)
 		send(cl.getFd(), "PRIVMSG <destinataire> <texte à envoyer>\r\n", 44, 0);
 	}
 }
-
-void	Error_argument(client cl, int cmd_type)
-{
-	if (cmd_type == 1)
-		send(cl.getFd(), "PASS <mdp>\r\n", 13, 0);
-	else if (cmd_type == 2)
-		send(cl.getFd(), "NICK <pseudonyme>\r\n", 20, 0);
-	else if (cmd_type == 3)
-		send(cl.getFd(), "USER <nom d'utilisateur> <hôte> <nom de serveur> :<nom réel>\r\n", 65, 0);
-	else if (cmd_type == 4)
-		send(cl.getFd(), "OPER <utilisateur> <mot de passe>\r\n", 36, 0);
-	else if (cmd_type == 5)
-		send(cl.getFd(), "JOIN <canal> [<mdp>]\r\n", 23, 0);
-	else if (cmd_type == 6)
-		send(cl.getFd(), "PRIVMSG <destinataire> <texte à envoyer>\r\n", 44, 0);
-	send(cl.getFd(), "If you need help : /HELP\r\n", 27, 0);
-}
--- srcs/commands/pass.cpp
+++ srcs/commands/pass.cpp
@@ -4,8 +4,10 @@
 
 void    cmd_pass(client cl, std::vector<std::string> cmd, server serv)
 {
-    if (cmd.size() != 2)
-        Error_argument(cl, 1);
+    if (cmd.size() < 2)
+        send(cl.getFd(), "ERR_NEEDMOREPARAMS \r\n", 22, 0);
+    else if (cl.isAccepted() == true)
+        send(cl.getFd(), "ERR_ALREADYREGISTRED \r\n", 24, 0);
     else if (cmd[1] == serv.getPassword())
         cl.pass = true;
     else
