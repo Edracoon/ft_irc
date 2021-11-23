71c5a6037ab2ffca7806a5adea2a3110bb4dda2c
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-11-23 16:01:46 +0100

    started feature join

---
+++ srcs/commands/join.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   join.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 15:49:07 by epfennig          #+#    #+#             */
+/*   Updated: 2021/11/23 16:01:35 by epfennig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../classes/client.hpp"
+#include "../classes/server.hpp"
+#include "../classes/parser.hpp"
+
+void	cmd_join(client* cli, std::vector<std::string>	cmd, server* serv)
+{
+	
+}
