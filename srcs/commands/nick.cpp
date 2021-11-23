f0246b3d983f47c22ec3b743ecff533de5bc3b3f
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-11-23 12:04:10 +0100

    connexion client improved

--- srcs/commands/nick.cpp
+++ srcs/commands/nick.cpp
@@ -1,10 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nick.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 11:59:30 by epfennig          #+#    #+#             */
+/*   Updated: 2021/11/23 11:59:31 by epfennig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../classes/client.hpp"
 #include "../classes/server.hpp"
 #include "../classes/parser.hpp"
 
 void    cmd_nick(client* cl, std::vector<std::string> cmd,  server serv)
 {
-    std::cout << "cmd[i] = '" << cmd[1] << "'" << std::endl;
     if  (cmd.size() < 2)
         send(cl->getFd(), "ERR_NONICKNAMEGIVEN\r\n", 22, 0);
     else if (serv.findClientByName(cmd[1]) != NULL)
