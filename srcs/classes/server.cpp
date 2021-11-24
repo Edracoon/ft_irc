02157c1dc0638779281171e086134fb1efece40b
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-11-24 11:47:47 +0100

    fixed segfault

--- srcs/classes/server.cpp
+++ srcs/classes/server.cpp
@@ -6,7 +6,7 @@
 /*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:44:37 by epfennig          #+#    #+#             */
-/*   Updated: 2021/11/24 11:29:08 by epfennig         ###   ########.fr       */
+/*   Updated: 2021/11/24 11:42:45 by epfennig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,7 +120,7 @@ void			server::deleteClient(client* tmp)
 				(*it)->curr_chan->deleteClientFromChan(*it);
 			std::cout << (*it)->getNickname() << " - " << (*it)->getUsername() << std::endl;
 			this->clients.erase(it);
-			delete *it;
+			delete tmp;
 			return ;
 		}
 	}
