84fe069b7920aed60efb4bed3d2feac29db57dea
Florent Gomez <fgomez@c2r4p1.42nice.fr>
2021-11-26 18:15:04 +0100

    JOIN upgrade

--- srcs/commands/join.cpp
+++ srcs/commands/join.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   join.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgomez <fgomez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 15:49:07 by epfennig          #+#    #+#             */
-/*   Updated: 2021/11/25 17:57:30 by epfennig         ###   ########.fr       */
+/*   Updated: 2021/11/26 18:11:26 by fgomez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,73 @@
 #include "../classes/parser.hpp"
 #include "../classes/channel.hpp"
 
-channel*	create_channel(client* cli, std::vector<std::string> cmd, server* serv)
+channel*	create_channel(client* cl, std::vector<std::string> cmd, server* serv)
 {
 	channel*	new_chan = new channel(cmd[1]);
-	new_chan->addClient(cli, cmd);
+	new_chan->addClient(cl, cmd);
 	serv->channels.push_back(new_chan);
-	cli->curr_chan = new_chan;
+	cl->curr_chan = new_chan;
 	return (new_chan);
 }
 
-void	cmd_join(client* cli, std::vector<std::string> cmd, server* serv)
+void	cmd_join(client* cl, std::vector<std::string> cmd, server* serv)
 {
 	channel*	curr_chan = NULL;
 	if (cmd.size() < 2)
-		send(cli->getFd(), (cmd[0] + " :Not enough parameters\r\n").c_str(), cmd[0].length() + 25, 0);
+		send(cl->getFd(), (cmd[0] + " :Not enough parameters\r\n").c_str(), cmd[0].length() + 25, 0);
 	else if (cmd[1][0] != '#' && cmd[1][0] != '&')
-		send(cli->getFd(), (cmd[1] + " :Bad Channel Mask\r\n").c_str(), cmd[1].length() + 20, 0);
+		send(cl->getFd(), (cmd[1] + " :Bad Channel Mask\r\n").c_str(), cmd[1].length() + 20, 0);
 	else
 	{
 		curr_chan = serv->findChannelByName(cmd[1]);
 
 		/* Leave Channel if Client is already in one */
-		if (cli->curr_chan != NULL)
+		if (cl->curr_chan != NULL)
 		{
-			cli->curr_chan->deleteClientFromChan(cli);
-			cli->curr_chan = NULL;
+			cl->curr_chan->deleteClientFromChan(cl);
+			cl->curr_chan = NULL;
 		}
 
 		/* Create chan or join it if its exists */
 		if (curr_chan == NULL)
 		{
-			curr_chan = create_channel(cli, cmd, serv);
-			curr_chan->printListUser(cli);
+			curr_chan = create_channel(cl, cmd, serv);
+			curr_chan->printListUser(cl);
 		}
 		else
 		{
-			if (curr_chan->addClient(cli, cmd) == 0)
+			if (curr_chan->addClient(cl, cmd) == 0)
 				return ;
-			cli->curr_chan = curr_chan;
-
-			/* Print List of Users in chan */
-			curr_chan->printListUser(cli);
+			cl->curr_chan = curr_chan;
+			sendToChan(cl);
 		}
 	}
+}
+
+void	norme_client(client* cl)
+{
+	// ==================== Confirm client join chan ===================
+	std::string msg = ":" + cl->getNickname() + "!" + cl->getUsername() + "@127.0.0.1 " + cl->getCurrMsg();
+	send(cl->getFd(), msg.c_str() , msg.length(), 0);
+	// ==================== Message for topic ==========================
+	msg = ":NiceIRC 332 " + cl->getNickname() + " " + cl->curr_chan->getName() + " :No Topic set";
+	send(cl->getFd(), (msg + "\r\n").c_str() , msg.length(), 0);
+	std::cout << msg << std::endl;
+	// ================== List Name ====================================
+	std::vector<client *>::iterator it = cl->curr_chan->users.begin();
+	std::vector<client *>::iterator ite = cl->curr_chan->users.end();
+
+	msg = ":NiceIRC 353 " + cl->getNickname() + " == " + cl->curr_chan->getName() + " :";
+	for ( ; it != ite ; it++)
+	{
+		msg += (*it)->getNickname() + " ";
+	}
+	msg.pop_back();
+	send(cl->getFd(), (msg + "\r\n").c_str(), msg.length() + 2, 0);
+	std::cout << msg << std::endl;
+	// ====================== RPL_END_OF_NAMES =========================
+	msg = ":NiceIRC 366 " + cl->getNickname() + " " + cl->curr_chan->getName() + " :End of NAMES list";
+	send(cl->getFd(), (msg + "\r\n").c_str(), msg.length() + 2, 0);
+	std::cout << msg << std::endl;
+
 }
