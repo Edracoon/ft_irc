20329384ceba62de8f4345f114ed1eee490810c4
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-12-02 16:01:19 +0100

    mode in progress

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -10,7 +10,7 @@ int check_mode(std::vector<std::string>& cmd, client* cl)
 	for (unsigned int j = 1 ; j < cmd[2].length() ; j++)
 	{
 		if (cmd[2][j] == 'o' || cmd[2][j] == 'k' || cmd[2][j] == 'v' || cmd[2][j] == 'l' || cmd[2][j] == 'i' || \
-			cmd[2][j] == 's' || cmd[2][j] == 'p' || cmd[2][j] == 'b' || cmd[2][j] == 'n' || cmd[2][j] == 't')
+			cmd[2][j] == 's' || cmd[2][j] == 'p' || cmd[2][j] == 'b' || cmd[2][j] == 'n' || cmd[2][j] == 't' || cmd[2][j] == 'm')
 			;
 		else
 		{
@@ -37,9 +37,22 @@ void	removeMode(client* cl, std::vector<std::string> cmd)
 
 void	addMode(client* cl, std::vector<std::string> cmd)
 {
-	for (unsigned int i = 1; i < cmd[2].size(); i++)
+	bool	mode_arg = false;
+	for (unsigned int i = 1; i < cmd[2].length(); i++)
 	{
-		if (cl->curr_chan->modes.find(cmd[2][i]) == std::string::npos)
+		if (!mode_arg && cmd[2][i] == 'k' || cmd[2][i] == 'l' || cmd[2][i] == 'b' || \
+						cmd[2][i] == 't' || cmd[2][i] == 'v' || cmd[2][i] == 'o')
+		{
+			if (cmd.size() < 4)
+				send_error_code(cl->getFd(), "696", cl->getNickname(), std::string(1, cmd[2][i]), ":You must specify a parameter for this mode.");
+			else if (1)
+				; /* handle parameters for mode */
+			mode_arg = true;
+		}
+		else if (mode_arg && cmd[2][i] == 'k' || cmd[2][i] == 'l' || cmd[2][i] == 'b' || \
+							cmd[2][i] == 't' || cmd[2][i] == 'v' || cmd[2][i] == 'o')
+			send_error_code(cl->getFd(), "666", cl->getNickname(), std::string(1, cmd[2][i]), ":Only one mode with parameter can be taken.");
+		else if (cl->curr_chan->modes.find(cmd[2][i]) == std::string::npos)
 			cl->curr_chan->modes.push_back(cmd[2][i]);
 	}
 }
@@ -56,9 +69,11 @@ void	cmd_mode(client* cl, std::vector<std::string> cmd, server* serv)
 		send_error_code(cl->getFd(), "403", cl->getNickname(), cmd[1], ":No such channel");
 	else if (cmd.size() == 2)
 		send_error_code(cl->getFd(), "324", cl->getNickname(), cl->curr_chan->getName() + " :+" + cl->curr_chan->modes, "");
+	else if (serv->findChannelByName(cmd[1])->isOperator(cl->getNickname()) == false)
+		send_error_code(cl->getFd(), "482", cl->getNickname(), cmd[1], ":You must have channel op access or above to set channel mode i");
 	else
 	{
-		check_mode(cmd, cl);
+		check_mode(cmd, cl); // ARG MODES -> [ +k <key> ] [ +l <max_user> ] [ +b <users_banlist> ] [ +t <topic> ] [ +v <user> ] [ +o <user> ]
 		if (cmd[2][0] == '+')
 		{
 			addMode(cl, cmd);
